5a622ffcfebe90d4f5447309564d79ae9bc1f85e
Tyks23 <61016508+Tyks23@users.noreply.github.com>
2021-02-07 18:48:07 +0200

    esimene kodutoo
    Lyhiks funktsioon, et teostada HTTP päring testimise endpointile.

---
+++ w01_funktsioon/SravaApp2/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.net.*;
+import java.io.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/albums")).build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+    }
+}
